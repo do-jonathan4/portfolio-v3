f268a52abc2121fc6586c40ca9da732098e7c926
John Do <d.johnt45@gmail.com>
2022-06-15 17:10:26 -0700

    added title to work pg

--- dist/js/work.js
+++ dist/js/work.js
@@ -3,16 +3,19 @@ const projects = document.querySelector('.projects')
 const projectsArray = [
   {
     img: 'img/projects/project1.jpg',
+    title: 'Chuck Norris Facts',
     link: 'https://do-jonathan4.github.io/ajax-project/',
     git: 'https://github.com/do-jonathan4/ajax-project'
   },
   {
     img: 'img/projects/project2.jpg',
+    title: 'Week Planner',
     link: 'https://do-jonathan4.github.io/week-planner/',
     git: 'https://github.com/do-jonathan4/week-planner'
   },
   {
     img: 'img/projects/project3.jpg',
+    title: 'Group Char App',
     link: 'https://group-chat-app1.herokuapp.com/',
     git: 'https://github.com/do-jonathan4/chat-app-project',
   }
@@ -25,6 +28,7 @@ for (let i of projectsArray) {
     <a href="${i.link}" target="_blank" class="btn-light">
       <img src="${i.img}" alt="project" srcset="">
     </a>
+    <p class="title">${i.title}</p>
     <a href="${i.git}" target="_blank" class="btn-dark">
       <i class="fab fa-github"></i> GitHub
     </a>
